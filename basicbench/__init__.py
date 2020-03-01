8e1875cb408874d0438513cbb0227c4a3598b59d
Carl Mundy <munc@tessella.co.uk>
2020-03-01 14:49:43 +0000

    Added Bench class

--- basicbench/__init__.py
+++ basicbench/__init__.py
@@ -1,5 +1,9 @@
-"""Top-level package for Basic Bench."""
+from .bench import Bench
 
 __author__ = """Carl Mundy"""
 __email__ = 'carl.j.mundy@gmail.com'
 __version__ = '0.1.0'
+
+__all__ = [
+    'Bench'
+]
---
+++ basicbench/bench.py
@@ -0,0 +1,5 @@
+class Bench:
+
+    def new(self):
+        """Return a new Experiment for usage"""
+        return 
