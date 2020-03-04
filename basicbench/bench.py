1307b86f9998bde840138d67a322d5118142c56b
Carl Mundy <carl.mundy@tessella.com>
2020-03-04 14:49:25 +0000

    Bench remembers experiments

--- basicbench/bench.py
+++ basicbench/bench.py
@@ -1,8 +1,17 @@
+from typing import List
+
 from .experiment import Experiment
 
 
 class Bench:
 
+    def __init__(self):
+        self._experiments = []
+
     def new(self, *args, **kwargs) -> Experiment:
         """Return a new Experiment for usage"""
         return Experiment(*args, **kwargs)
+
+    def list(self) -> List[Experiment]:
+        return self._experiments
+
--- tests/test_bench.py
+++ tests/test_bench.py
@@ -7,3 +7,7 @@ from . import utils
 
 def test_can_create_new_bench_from_class():
     bench = utils.create_bench()
+
+def test_bench_remembers_experiments():
+    bench = utils.create_bench()
+    assert bench.list() == []
